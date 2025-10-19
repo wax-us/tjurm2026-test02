01de6cd81c6db6d3c7476d5f4c8579dbd47b17c8
wax-us <wyx1201060036@163.com>
2025-10-19 23:01:29 +0800

    完成4/9

--- src/find_contours/impl.cc
+++ src/find_contours/impl.cc
@@ -1,4 +1,5 @@
 #include "impls.h"
+#include <iostream>
 
 
 std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input) {
@@ -17,7 +18,19 @@ std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input) {
      * 运行测试点，你找到的轮廓与答案的轮廓一样就行。
      */
     
-    std::vector<std::vector<cv::Point>> res;
+    std::vector<std::vector<cv::Point>> res,res1;
+    cv::Mat temp1,temp2;
+    std::vector<cv::Vec4i> hierarchy;//[0]下一个 [1]上一个 [2]第一个子 [3]父
     // IMPLEMENT YOUR CODE HERE
+    cv::cvtColor(input, temp1 ,cv::COLOR_BGR2GRAY);
+    cv::threshold(temp1,temp2,100,255,cv::THRESH_BINARY);
+    cv::findContours(temp2,res1,hierarchy,3,2);
+    for(int i=0;i<res1.size();i++){
+        if(hierarchy[i][2] == -1){
+            res.push_back(res1[i]);
+            //std::cout<<hierarchy[i]<<std::endl;
+        }
+    }
+
     return res;
 }
